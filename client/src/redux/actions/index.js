13361ad33179b43ffb9331fe1eca4fe72c682ae2
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-07-06 00:21:56 -0300

    Added filter by name to actions/ and reducer/

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,11 +1,19 @@
 import axios from "axios";
-import players from "../../players.json"
+import players from "../../players.json";
 
 export const GET_ALL_PLAYERS = "GET_ALL_PLAYERS";
+export const ORDER_USERS_BY_NAME = "ORDER_USERS_BY_NAME";
 
-export const getAllPlayers =()=>{
-    return{
-        type:GET_ALL_PLAYERS,
-        payload: players
-    }
-}
+export const getAllPlayers = () => {
+  return {
+    type: GET_ALL_PLAYERS,
+    payload: players,
+  };
+};
+
+export const orderUsersByName = ({ order }) => {
+  return {
+    type: ORDER_USERS_BY_NAME,
+    payload: order,
+  };
+};
