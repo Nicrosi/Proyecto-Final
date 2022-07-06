a2c1174ac51ebd3191d19e4f4216ce32e0dea9a9
DianReyna <reynaarmas@gmail.com>
2022-07-06 11:30:53 -0500

    refactor model/Team

--- api/src/models/Team.js
+++ api/src/models/Team.js
@@ -4,7 +4,7 @@ module.exports = (sequelize) => {
   sequelize.define(
     "team",
     {
-      id: {
+      id_Team: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
