a7dce915558209fabfdf61c32f35beba0bd57511
Guillermo Nicrosi <erknardsieghartrage@gmail.com>
2022-07-06 17:39:16 -0300

    feat(UserRouter/post)

---
+++ api/src/routes/UserRouter.js
@@ -0,0 +1,44 @@
+const {Router} = require('express');
+const router = Router();
+const {get_Userdb} = require('../utils/User_Controllers');
+const {User} = require('../db');
+
+router.post('/', async (req, res) => {
+    let{
+        dni,
+        name,
+        last_name,
+        is_admin,
+        e_mail,
+        phone,
+        num_contact,
+        picture,
+        gender
+    } = req.body;
+
+    try{
+        let findUser = await User.findall({
+            where:{
+                dni: dni
+            }
+        })
+        if(findUser.length > 0){
+            res.status(403).send("User could not be created, DNI already in use");
+        }else{
+            await User.create({
+                dni,
+                name,
+                last_name,
+                is_admin,
+                e_mail,
+                phone,
+                num_contact,
+                picture,
+                gender
+            });
+            res.send('Created!');
+        }
+    }catch(err){
+        console.log(err)
+    }
+})
