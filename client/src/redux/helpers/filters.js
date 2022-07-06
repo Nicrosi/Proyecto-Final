a600105c2e4f48a3c33a826c9257d6baa28ae83a
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-07-06 14:58:37 -0300

    refactor(src/redux/helpers): refactored filterByGender function

--- client/src/redux/helpers/filters.js
+++ client/src/redux/helpers/filters.js
@@ -1,9 +1,6 @@
 export const filterByGender = (arr = [], gender = "all") => {
   if (gender === "all") return arr;
   else {
-    return arr.filter((player) => {
-      console.log(player.gender, gender);
-      return player.gender === gender;
-    });
+    return arr.filter((player) => player.gender === gender);
   }
 };
