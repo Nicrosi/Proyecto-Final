d8a92d0b3189fe994ccb161d6874ad57a06850b8
DianReyna <reynaarmas@gmail.com>
2022-07-07 12:40:25 -0500

    feat(api/routes) add endpoint GET /category

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,11 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
 const router = Router();
-
+const category = require("../routes/Category.routes.js");
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use("/category", category);
 
 module.exports = router;
