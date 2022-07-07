3d064833091f0aa50ae1d06ba33764088173a92b
DemonTwo <76266733+DemonTwo@users.noreply.github.com>
2022-07-07 19:32:25 -0300

    refactor: (User_Controllers.js, UserRouter.js) find user by name name all user

--- api/src/models/Subtournament.js
+++ api/src/models/Subtournament.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  sequelize.define('subtournament', {
+  sequelize.define('subTournament', {
     id_subt: {
       type: DataTypes.INTEGER,
       autoIncrement: true, 
