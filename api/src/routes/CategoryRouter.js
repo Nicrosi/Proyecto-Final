1d7603b318de7ab06713818905b985cbf855adac
DianReyna <reynaarmas@gmail.com>
2022-07-07 16:36:45 -0500

    refactor routes/CategoryRouter

--- api/src/routes/CategoryRouter.js
+++ api/src/routes/CategoryRouter.js
@@ -1,7 +1,23 @@
-const { getCategory } = require("../utils/Category_Controllers.js");
 const { Router } = require("express");
 const router = Router();
+const { category } = require("../utils/Category_Controllers.js");
 
-router.get("/", getCategory);
+router.get("/", async (req, res, next) => {
+  try {
+    // const newScore = await Score.create({
+    //   previous_tournaments: 0,
+    //   hit_knowledge: 2,
+    //   other_strokes: 1,
+    //   special_hits: 0,
+    //   kick_serve_control: 1,
+    //   game_strategy: 0,
+    // }); // momentaneo
+
+    const allCategory = await category();
+    res.json(allCategory);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
