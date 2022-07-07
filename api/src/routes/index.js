ef3a6462f292d1431d355c15a2bcd6bc0aa85cde
Ivan Ignacio Peña Mattos <76266733+DemonTwo@users.noreply.github.com>
2022-07-07 15:44:24 -0300

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,7 +2,7 @@
 const { Router } = require('express');
 const RouteUser = require('../routes/UserRouter')
 const router = Router();
-const category = require("../routes/Category.routes.js");
+const category = require("../routes/CategoryRouter.js");
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
