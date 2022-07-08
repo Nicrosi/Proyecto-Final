e5b0c158c0b440e554aba8f3942ef5ef6e4bf321
Guillermo Nicrosi <erknardsieghartrage@gmail.com>
2022-07-08 00:18:36 -0300

    feat(routes/GET Score)

--- api/src/routes/ScoreRouter.js
+++ api/src/routes/ScoreRouter.js
@@ -1,10 +1,38 @@
 const {Router} = require('express');
 const router = Router();
+const {Score, User} = require('../db');
 
 router.get('/', async (req, res) => {
 
 });
 
-router.post('/', async (req, res) => {
-
+router.post('/:dni', async (req, res) => {
+    let {dni} = req.params;
+    let{
+        previous_tournaments,
+        hit_knowledge,
+        other_strokes,
+        special_hits,
+        kick_serve_control,
+        game_strategy
+    } = req.body;
+    try{
+        let newScore = await Score.create({
+            previous_tournaments,
+            hit_knowledge,
+            other_strokes,
+            special_hits,
+            kick_serve_control,
+            game_strategy
+        });
+        let user = await User.findAll({
+            where : {
+                dni: dni
+            }
+        });
+        newScore.addScore(user);
+        res.status(200).send('Sent!');
+    }catch(err){
+        console.log(err);
+    }
 });
