3753e4f95e6cb43016d3bd8eae93bdf4a5816b2e
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-07-09 00:21:06 -0300

    feat(components/Gallery): add Gallery.jsx and its images

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -9,6 +9,38 @@ export default function LandingPage() {
       <section>
         <Carousel />
       </section>
+      {/* <section
+        className="d-flex flex-column align-items-center"
+        style={{ minHeight: "75vh" }}
+      >
+        <h1 className="mt-4 ">How to take part?</h1>
+        <Steps />
+      </section> */}
+      <section
+        className="d-flex flex-column align-items-center bg-secondary"
+        style={{ minHeight: "100vh" }}
+      >
+        <h1 className="mt-4 mb-5">Participants</h1>
+      </section>
+      <section
+        className="d-flex flex-column align-items-center"
+        style={{ minHeight: "100vh", border: "solid 1px black" }}
+      >
+        <h1 className="mt-4 mb-5">Galery</h1>
+        <Gallery />
+      </section>
+      <section
+        className="d-flex flex-column align-items-center"
+        style={{ minHeight: "100vh", border: "solid 1px black" }}
+      >
+        <h1 className="mt-4 mb-5">Frequent questions</h1>
+      </section>
+      <section
+        className="d-flex flex-column align-items-center"
+        style={{ minHeight: "100vh", border: "solid 1px black" }}
+      >
+        <h1 className="mt-4 mb-5">Sponsors</h1>
+      </section>
     </>
   );
 }
