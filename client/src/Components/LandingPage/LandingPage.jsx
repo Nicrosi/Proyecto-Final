12d235860e60308e25fedb3067880c30ed97c094
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-07-08 22:27:30 -0300

    feat(client/src/components): Add LandingPage.jsx

---
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Carousel from "../ControlledCarousel/ControlledCarousel";
+import Gallery from "../Gallery/Gallery";
+import Steps from "../Steps/Steps";
+
+export default function LandingPage() {
+  return (
+    <>
+      <section></section>
+    </>
+  );
+}
