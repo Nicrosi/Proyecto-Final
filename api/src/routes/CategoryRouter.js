ac602e9cbbc693dd00d581f3fa72d97fa404dbf4
DianReyna <reynaarmas@gmail.com>
2022-07-09 16:51:20 -0500

    refactor(api/routes/CategoryRouter - api/utils/Category_Controller) Get?id_category=...

--- api/src/routes/CategoryRouter.js
+++ api/src/routes/CategoryRouter.js
@@ -3,9 +3,17 @@ const router = Router();
 const { category } = require("../utils/Category_Controllers.js");
 
 router.get("/", async (req, res, next) => {
+  const { id_category } = req.query;
   try {
-    const allCategory = await category();
-    res.json(allCategory);
+    if (id_category) {
+      const user_category = await category(id_category);
+      user_category
+        ? res.json(user_category)
+        : res.status(404).send("Category doesn't exist!");
+    } else {
+      const allCategory = await category();
+      res.json(allCategory);
+    }
   } catch (error) {
     next(error);
   }
