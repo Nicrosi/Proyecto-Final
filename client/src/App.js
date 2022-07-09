440c71413195fe3b150140d53aba69485c3c7674
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-07-08 22:29:24 -0300

    feat(client/src): add route of LandingPage in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,18 @@
-
-import React from 'react';
-import { Route} from 'react-router-dom';
-import DetailsPlayer from './components/DetailsPLayer/DetailsPlayer.jsx';
-import HomeAdmin from './components/HomeAdmin/HomeAdmin.jsx';
-import NavBar from './components/NavBar/NavBar';
-import { Players } from './components/Players/Players.jsx';
-
+import React from "react";
+import { Route } from "react-router-dom";
+import DetailsPlayer from "./components/DetailsPLayer/DetailsPlayer.jsx";
+import HomeAdmin from "./components/HomeAdmin/HomeAdmin.jsx";
+import LandingPage from "./components/LandingPage/LandingPage.jsx";
+import NavBar from "./components/NavBar/NavBar";
+import { Players } from "./components/Players/Players.jsx";
 
 function App() {
-
   return (
-   
     <div className="App">
-      {/* <Route exact path={"/"} component={LandingPage} /> */}
       <Route path={"/"} component={NavBar} />
+      <Route exact path={"/"} component={LandingPage} />
       <Route exact path={"/players"} component={Players} />
-      <Route exact path={"/HomeAdmin"} component={HomeAdmin}/>
+      <Route exact path={"/HomeAdmin"} component={HomeAdmin} />
       <Route exact path={"/players/:playerId"} component={DetailsPlayer} />
     </div>
   );
