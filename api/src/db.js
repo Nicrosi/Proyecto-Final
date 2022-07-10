291d9da7907608ad9fec54779937d27a0c351baf
sebacontegrand <sebacontegarnd@gmail.com>
2022-07-09 23:55:27 -0300

    feat(add component Tornament/TournamentsAllAgainst/TournamentsSimpleEl)

--- api/src/db.js
+++ api/src/db.js
@@ -27,7 +27,12 @@ modelDefiners.forEach(model => model(sequelize));
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
-
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully to db.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const { User, Team, Match, Round, Inscription, Tournament, Subtournament, Score, Category, Sponsor } = sequelize.models;
