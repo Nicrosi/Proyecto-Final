e0178bdd547d5c72422cc83724cad234f995ba05
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-07-11 01:28:47 -0300

    refactor(varius): refactoring Users.jsx, filters.js, SearchBar.jsx, HomeAdmin.jsx

--- client/src/redux/helpers/filters.js
+++ client/src/redux/helpers/filters.js
@@ -2,10 +2,10 @@ export const filterUsers = (arr = [], gender = "all", category = "all") => {
   if (gender !== "all") {
     arr = arr.filter((user) => user.gender === gender);
   }
-  // if (category !== "all") { //aun no me trae categorias del back
-  //   arr = arr.filter(
-  //     (user) => user.category.type === category.toLowerCase()
-  //   );
-  // }
+  if (category !== "all") {
+    arr = arr.filter(
+      (user) => user.category?.type.toLowerCase() === category.toLowerCase()
+    );
+  }
   return arr;
 };
