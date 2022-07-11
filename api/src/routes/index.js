28d51ace32747e332d27281d41bd353687c674d1
Diego Fernando Rojas Carrillo <drawingsdf@gmail.com>
2022-07-11 15:59:47 -0500

    feat(api/routes): tournament routes created

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,13 @@
+
 const { Router } = require('express');
 const RouteUser = require('../routes/UserRouter')
 const category = require("../routes/CategoryRouter.js");
 const Sponsor = require("../routes/SponsorRouter.js");
 const Score = require("../routes/ScoreRouter.js");
 const Subtournament = require("../routes/SubtRouter.js");
+const Tournament = require("../routes/TournamentRouter.js");
 const router = Router();
-
+ 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -17,5 +19,6 @@ router.use('/user', RouteUser);
 router.use('/sponsor', Sponsor);
 router.use('/score', Score);
 router.use('/subtournament', Subtournament);
+router.use('/tournament', Tournament);
 
 module.exports = router;
