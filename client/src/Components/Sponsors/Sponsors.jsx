877ff1a604274374ecb80410cd1e0a6ebec26e4a
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-07-10 21:39:10 -0300

    feat(src/components/Sponsors): add connection with redux

--- client/src/Components/Sponsors/Sponsors.jsx
+++ client/src/Components/Sponsors/Sponsors.jsx
@@ -1,30 +1,44 @@
+import { useEffect } from "react";
 import { Container, Col, Row } from "react-bootstrap";
-import spn1 from "../../img/sponsor1.png";
-import spn2 from "../../img/sponsor2.png";
-import spn3 from "../../img/sponsor3.png";
-import spn4 from "../../img/sponsor4.png";
-import spn5 from "../../img/sponsor5.png";
-import spn6 from "../../img/sponsor6.png";
-import spn7 from "../../img/sponsor7.png";
-import spn8 from "../../img/sponsor8.png";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllSponsors } from "../../redux/actions";
 
-const arrSponsors = [spn1, spn2, spn3, spn4, spn5, spn6, spn7, spn8];
 export default function Sponsors() {
+  const dispatch = useDispatch(),
+    sponsors = useSelector((state) => state.sponsors);
+
+  useEffect(() => {
+    dispatch(getAllSponsors());
+  }, [dispatch]);
+
   return (
     <Container fluid className="py-5">
-      <Row className="justify-content-around align-content-around">
-        {arrSponsors.map((spn, i) => (
-          <Col
-            xs={6}
-            sm={3}
-            lg={1}
-            key={i}
-            className="d-flex justify-content-center"
-          >
-            <img src={spn} alt={`${i}`} width={80} height={80} />
-          </Col>
-        ))}
-      </Row>
+      {sponsors.length > 0 ? (
+        <Row className="justify-content-around align-content-around">
+          {sponsors.map((spn, i) => (
+            <Col
+              xs={6}
+              sm={3}
+              lg={1}
+              key={i}
+              className="d-flex justify-content-center"
+              style={{ width: "fit-content" }}
+            >
+              <a href={spn.link} target="_blank">
+                <img
+                  src={spn.logo}
+                  alt={`${i}`}
+                  style={{ maxHeight: "120px", width: "auto" }}
+                />
+              </a>
+            </Col>
+          ))}
+        </Row>
+      ) : (
+        <h1 className="text-white" style={{ textAlign: "center" }}>
+          No sponsors found
+        </h1>
+      )}
     </Container>
   );
 }
