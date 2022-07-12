3e4d93f909c713b389e29048ecb6c4f475cca255
Diego <jdapuy@gmail.com>
2022-07-12 16:34:11 -0600

    clent/src/components/FormScore-FormSponsor-FormUser-FormTournament: Se hacen las validaciones de todos los forms

--- client/src/components/Tournament/FormTournament.jsx
+++ client/src/components/Tournament/FormTournament.jsx
@@ -1,10 +1,39 @@
 import React, { useState } from "react";
 import axios from "axios";
 import img1 from '../../assets/fototorneo.jpg'
+
+
+export function validate(input) {
+  let error = {};
+  
+  if (!input.date) {
+    error.company = "Name is required";
+  } else if (
+    !/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/.test(input.date) &&
+    input.date
+  ) {
+    error.date = "Invalid date format";
+  }
+
+  if (!input.location) {
+    error.location = "location is required";
+  } else if (input.location.length > 255) {
+    error.location = "Enter less than 255 characters";
+  }
+
+  return error;
+}
+
 export const FormTournament = () => {
+  const noError = "Looks good"
   const [input, setInput] = useState({
     date: "",
     location: "",
+  });
+
+  const [error, setError] = useState({
+    date: "date is required",
+    location: "location is required",
   });
 
   async function handleOnSubmit(e) {
@@ -19,6 +48,8 @@ export const FormTournament = () => {
       [e.target.name]: e.target.value,
     });
     
+    let objError = validate({ ...input, [e.target.name]: e.target.value });
+    setError(objError);
   }
 
   return (
@@ -54,10 +85,18 @@ export const FormTournament = () => {
                     type="date"
                     onChange={(e) => handleOnChange(e)}
                     placeholder="Tournaments Date..."
-                    name="Date"
+                    name="date"
                     value={input.name}
                     required
-                  />
+                    className={error.date?"form-control is-invalid":"form-control is-valid"}
+                  />{error.date ?
+                    <div id="validationServerUsernameFeedback" className="invalid-feedback">
+                    {error.date}
+                    </div>:
+                    <div id="validationServerUsernameFeedback" className="valid-feedback">
+                    {noError}
+                    </div>
+                  }
                 </div>
               </li>
 
@@ -71,17 +110,28 @@ export const FormTournament = () => {
                     placeholder="Tournaments Location..."
                     name="location"
                     value={input.location}
-                    required
-                  />
+                    className={error.location?"form-control is-invalid":"form-control is-valid"}
+                  />{error.location ?
+                    <div id="validationServerUsernameFeedback" className="invalid-feedback">
+                    {error.location}
+                    </div>:
+                    <div id="validationServerUsernameFeedback" className="valid-feedback">
+                    {noError}
+                    </div>
+                  }
                 </div>
               </li>
             </ul>
           </div>
 
           <div className="d-grid gap-2 mb-3" style={{ width: "90%" }}>
-            <button className="btn btn-success" type="submit">
-              Create
-            </button>
+          {Object.keys(error).length > 0 ? (
+              <button className="btn btn-secondary" style={{ backgroundColor: "#A7D129" }} type="submit" disabled>
+                Create
+              </button>
+            ) : (
+              <button className="btn btn-success" style={{ backgroundColor: "#A7D129" }} type="submit">Create</button>
+            )}
           </div>
         </form>
       </div>
