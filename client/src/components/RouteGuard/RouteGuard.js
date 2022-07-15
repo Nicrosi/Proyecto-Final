320867a2ddd506c5b5ec1def88dcd7487ea866c7
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-07-15 13:56:02 -0300

    feat(src/components/RouteGuard): add component

---
+++ client/src/components/RouteGuard/RouteGuard.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Redirect, useHistory } from "react-router-dom";
+import { verifyUser } from "../../redux/actions/authorization";
+
+const RouteGuard = ({ component: Component, ...rest }) => {
+  const loggedIn = useSelector((state) => state.auth.loggedIn);
+  const authChecked = useSelector((state) => state.auth.authChecked);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.currentUser);
+
+  useEffect(() => {
+    dispatch(verifyUser());
+  }, [dispatch]);
+
+  return (
+    <>
+      {authChecked ? (
+        <Route
+          {...rest}
+          render={(props) =>
+            loggedIn && user.is_admin === rest.admin ? (
+              <Component {...props} />
+            ) : (
+              <Redirect to="/login" />
+            )
+          }
+        />
+      ) : null}
+    </>
+  );
+};
+
+export default RouteGuard;
