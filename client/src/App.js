7301cd4613aff04bb45d8ef7a0dca6f4ecdf878e
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-07-15 22:08:12 -0300

    refactor(client/src): changed user path permissions.

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import DetailsUser from "./components/DetailsUser/DetailsUser.jsx";
 import LandingPage from "./components/LandingPage/LandingPage.jsx";
 import HomeAdmin from "./components/HomeAdmin/HomeAdmin.jsx";
 import NavBar from "./components/NavBar/NavBar";
-import { Inscription }  from "./components/Inscription/Inscription.jsx";
+import { Inscription } from "./components/Inscription/Inscription.jsx";
 import { Users } from "./components/Users/Users.jsx";
 import { FormScore } from "./components/FormScore/FormScore.jsx";
 import { FormSponsor } from "./components/FormSponsor/FormSponsor.jsx";
@@ -19,7 +19,7 @@ function App() {
   return (
     <div className="App">
       <Route path={"/"} component={NavBar} />
-      <RouteGuard exact path={"/Users"} component={Users} admin={true} />
+      <RouteGuard exact path={"/Users"} component={Users} admin={false} />
       <RouteGuard
         exact
         path={"/HomeAdmin"}
@@ -28,15 +28,24 @@ function App() {
       />
       {/* <Route exact path={"/HomeAdmin"} component={HomeAdmin} /> */}
       <Route exact path={"/"} component={LandingPage} />
-      <Route exact path={"/Users/:userId"} component={DetailsUser}/>
+      <Route exact path={"/Users/:userId"} component={DetailsUser} />
       <Route exact path={"/CreateScore/:userId"} component={FormScore} />
       <Route exact path={"/sponsor"} component={FormSponsor} />
       <Route exact path={"/tournament"} component={FormTournament} />
-      <Route exact path={"/inscription/:tournament_id"} component = {Inscription}/>
+      <Route
+        exact
+        path={"/inscription/:tournament_id"}
+        component={Inscription}
+      />
       <Route exact path={"/login"} component={LogIn} />
       <Route exact path={"/cpanel"} component={ControlPanel} />
       <Route exact path={"/SignIn"} component={FormUserRegister} />
-      <RouteGuard exact path={"/Profile/:userId"} component={ProfileUser} admin={false} />
+      <RouteGuard
+        exact
+        path={"/Profile/:userId"}
+        component={ProfileUser}
+        admin={false}
+      />
     </div>
   );
 }
