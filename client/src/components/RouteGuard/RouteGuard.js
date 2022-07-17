4f6ee7b69a67ea83aa731a1c8e87995f40b0a185
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-07-16 21:10:43 -0300

    refactor(src/components/RouteGuard): remove unnecesary useEffect

--- client/src/components/RouteGuard/RouteGuard.js
+++ client/src/components/RouteGuard/RouteGuard.js
@@ -1,17 +1,11 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
-import { verifyUser } from "../../redux/actions/authorization";
 
 const RouteGuard = ({ component: Component, ...rest }) => {
   const loggedIn = useSelector((state) => state.auth.loggedIn);
   const authChecked = useSelector((state) => state.auth.authChecked);
-  const dispatch = useDispatch();
   const user = useSelector((state) => state.auth.currentUser);
-
-  useEffect(() => {
-    dispatch(verifyUser());
-  }, [dispatch]);
 
   return (
     <>
