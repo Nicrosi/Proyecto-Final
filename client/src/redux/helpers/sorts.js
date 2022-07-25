eb13614d17420ff68b34a94380ee4970b0e5b452
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-07-25 00:09:52 -0300

    fix(src/redux/helpers): fix sorts.js

--- client/src/redux/helpers/sorts.js
+++ client/src/redux/helpers/sorts.js
@@ -1,7 +1,11 @@
 export const sortByName = (arr, order = "ascendent") => {
   const newArr = [...arr];
   if (order === "ascendent") {
-    return newArr.sort((a, b) => (a.name > b.name ? 1 : -1));
+    return newArr.sort((a, b) =>
+      a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+    );
   }
-  return newArr.sort((a, b) => (a.name > b.name ? -1 : 1));
+  return newArr.sort((a, b) =>
+    b.name.toLowerCase().localeCompare(a.name.toLowerCase())
+  );
 };
