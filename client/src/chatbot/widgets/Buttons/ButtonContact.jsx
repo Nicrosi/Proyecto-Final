7eca4130dd61772cb157cb9551d49d7035022749
Jose Nahuel Toloza <nahueltoloza014@gmail.com>
2022-07-25 00:13:59 -0300

    feat(src/chatbot): add wpp button in contact OE

--- client/src/chatbot/widgets/Buttons/ButtonContact.jsx
+++ client/src/chatbot/widgets/Buttons/ButtonContact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Button.module.css";
 import mailIcon from "../../../img/correo-icon.png";
 import phoneIcon from "../../../img/phone-icon.png";
+import wppIcon from "../../../img/wpp-icon.png";
 
 const ButtonContact = () => {
   return (
@@ -14,6 +15,11 @@ const ButtonContact = () => {
       <a href="tel:942033303">
         <button className={styles.btnTel}>
           <img src={phoneIcon} alt="phone" />
+        </button>
+      </a>
+      <a href="https://api.whatsapp.com/send?phone=0123456789" target="_blank">
+        <button className={styles.btnTel}>
+          <img src={wppIcon} alt="wpp" style={{ width: "23px" }} />
         </button>
       </a>
     </div>
