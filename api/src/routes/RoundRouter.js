ef2956fff15b253d2a2d17284e5643939822932e
Guillermo Nicrosi <erknardsieghartrage@gmail.com>
2022-07-26 18:44:46 -0300

    refactor(added try catch to new routes, added initialized attribute to subt model)

--- api/src/routes/RoundRouter.js
+++ api/src/routes/RoundRouter.js
@@ -1,32 +1,39 @@
 const { Router } = require("express");
 const router = Router();
-const { Round, User, Inscription, Team, Match } = require("../db");
+const { Round, User, Inscription, Team, Match, Subtournament } = require("../db");
 
 
 router.post('/firstround/:id_subt', async (req, res) => {
-    let {id_subt} = req.params;
-    let array_match = [];
-    let subt_teams = await Team.findAll({
-        attributes: ['id_team'],
-        include: {
-            model: User,
-            attributes: ['name'],
+    const {id_subt} = req.params;
+    try {
+        let array_match = [];
+        let subt_teams = await Team.findAll({
+            attributes: ['id_team'],
             include: {
-                model: Inscription,
-                attributes: ['id_user'],
-                where: {
-                    id_subt: id_subt
+                model: User,
+                attributes: ['name'],
+                include: {
+                    model: Inscription,
+                    attributes: ['id_user'],
+                    where: {
+                        id_subt: id_subt
+                    }
                 }
             }
-        }
-    });
+        });
     let filteredTeams = subt_teams.filter(s => s.dataValues.users.length > 0)
     let new_round = await Round.create({
         round_numb: 1,
         id_subt: id_subt
     });
+    await Subtournament.update({ 
+        initialized: true,
+        where: {
+            id_subt: id_subt
+        }
+    });
     let matches = {
-        score: '[0,0]',
+        score: '[[0,0,0],[0,0,0]]',
         id_round: new_round.dataValues.id_round
     }
     let id_teams = filteredTeams.map(x => x.dataValues.id_team);  
@@ -44,7 +51,10 @@ router.post('/firstround/:id_subt', async (req, res) => {
         new_match[i].addTeam(team_pairs[i][0]);
         new_match[i].addTeam(team_pairs[i][1]);
     }
-    res.send('se re posteo perro');
+    res.send('Round created successfully');
+    } catch (error) {
+        console.log(error);
+    }    
 })
 
 // router.post('/nextround/:id_subt', async (req, res) => {
