8ec7801240cb547e27d2400bd97dc7561038704d
DeniseCardozo <dennicardozo96@gmail.com>
2022-07-27 13:41:12 -0300

    refactor(client/src/components/DetailsUser - /UserCard - /Users): aplicado de modal para mostrar detailsUser

--- client/src/components/UserCard/UserCard.jsx
+++ client/src/components/UserCard/UserCard.jsx
@@ -1,20 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styles from "./UserCard.module.css";
-export const UserCard = ({
-  dni,
-  name,
-  last_name,
-  picture,
-  gender,
-  category,
-}) => {
+
+
+export const UserCard = ({ user, setDataModal}) => {
+  function handleOpenDetail(e) { 
+    e.preventDefault();
+    setDataModal(user);
+  }
+
   return (
-    <Link
-      to={`/Users/${dni}`}
-      style={{ textDecoration: "none", color: "black" }}
-    >
-      <div className={`card ${styles.card}`} style={{ height: "100%" }}>
+      <div className={`card ${styles.card}`} style={{ height: "100%" }}  data-bs-toggle="modal" data-bs-target="#staticBackdrop" 
+      onClick={(e)=>handleOpenDetail(e)}>
         <div
           className="d-flex flex flex-md-row flex-lg-row"
           style={{ height: "100%" }}
@@ -22,16 +18,16 @@ export const UserCard = ({
           <div className={`${styles.pictureContainer} col-md-5 col-sm-3`}>
             <img
               className={`${styles.picture} rounded-start`}
-              src={picture}
+              src={user.picture}
               alt="img"
             />
           </div>
           <div className="col-md-7 col-sm-5">
             <div className="card-body">
-              <h5 className="card-title">{`${name} ${last_name}`}</h5>
-              <p className="card-text mt-2 mb-0">Gender: {gender}</p>
-              {category ? (
-                <p className="card-text">Category: {category.type}</p>
+              <h5 className="card-title">{`${user.name} ${user.last_name}`}</h5>
+              <p className="card-text mt-2 mb-0">Gender: {user.gender}</p>
+              {user.category ? (
+                <p className="card-text">Category: {user.category.type}</p>
               ) : (
                 <p className="card-text">Category: None</p>
               )}
@@ -39,6 +35,5 @@ export const UserCard = ({
           </div>
         </div>
       </div>
-    </Link>
   );
 };
--- client/src/components/Users/Users.jsx
+++ client/src/components/Users/Users.jsx
@@ -5,9 +5,11 @@ import { Row, Col } from "react-bootstrap";
 import Filter from "../Filter/Filter";
 import { UserCard } from "../UserCard/UserCard";
 import Navigator from "../Navigator/Navigator";
+import DetailsUser from "../DetailsUser/DetailsUser";
 
 export const Users = () => {
   const [currentPage, setCurrentPage] = useState(0);
+  const [dataModal, setDataModal] = useState({});
   const users = useSelector((state) => state.rootReducer.filteredUsers);
   const dispatch = useDispatch();
 
@@ -24,19 +26,45 @@ export const Users = () => {
       <div style={{ width: "100%" }}>
         <Row className="g-3 mx-3 mt-2">
           {users.length > 0 ? (
-            users.slice(11 * currentPage, 11 * currentPage + 11).map((p) => {
+            users.slice(11 * currentPage, 11 * currentPage + 11).map((user) => {
               return (
-                <Col lg={4} md={6} sm={12} key={p.dni}>
+                <Col lg={4} md={6} sm={12} key={user.dni}>
                   <UserCard
-                    key={p.dni}
-                    dni={p.dni}
-                    name={p.name}
-                    last_name={p.last_name}
-                    picture={p.picture}
-                    gender={p.gender}
-                    category={p.category}
+                    user={user}
+                    setDataModal={setDataModal}
                   />
+                                  <div
+                  className="modal fade"
+                  id="staticBackdrop"
+                  data-bs-backdrop="static"
+                  data-bs-keyboard="false"
+                  tabIndex="-1"
+                  aria-labelledby="staticBackdropLabel"
+                  aria-hidden="true"
+                >
+                  <div className="modal-dialog modal-dialog-centered modal-xl">
+                    <div
+                      className="modal-content"
+                      style={{ backgroundColor: "rgb(43, 43, 44)" }}
+                    >
+                      <div className="modal-header">
+                        <button
+                          type="button"
+                          className="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+                      (
+                      <div>
+                      <DetailsUser dataModal={dataModal} />
+                      </div>
+                      )
+                    </div>
+                  </div>
+                </div>
                 </Col>
+                
               );
             })
           ) : (
