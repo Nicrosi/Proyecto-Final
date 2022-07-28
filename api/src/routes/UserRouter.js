207a0e272f2ce79ebb5b0ab8ffc68fd466b823f9
Guillermo Nicrosi <65926750+DarkRageTM@users.noreply.github.com>
2022-07-28 10:10:23 -0300

    Update UserRouter.js

--- api/src/routes/UserRouter.js
+++ api/src/routes/UserRouter.js
@@ -76,9 +76,6 @@ router.post('/', async (req, res) => {
               dni: dni
           }
         })
-        if(name === 'Admin'){
-          is_admin=true;
-        }
         if(findUser.length > 0){
             res.status(403).send("User could not be created, DNI already in use");
         }else{
